186c077181b285cf25083804fc475f7a06a6af24
Kishana Liburd <kishana.liburd@gmail.com>
2020-10-29 17:59:23 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+require 'pry'
+sentence = ("Hello there, and how are you?")
+def reverse_each_word(sentence)
+  new_array = []
+  another_array = []
+  #binding.pry
+  sentence.split(" ").collect do |words|
+    new_array << words.reverse
+    #new_array.each do |reverse_sentence|
+#another_array << reverse_sentence.reverse
+#new_array.join(" ")
+end
+return new_array.join(" ")
+end
+
+
+
+
+
